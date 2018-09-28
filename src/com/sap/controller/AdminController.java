85a2ebb5effc6440c24b329ca612a218675cae60
Rentz <derick.rentz@sap.com>
2018-09-28 13:52:04 -0300

    added verification if user can make an action, and if it can't, the application will be redirected to accessDenied page

--- src/com/sap/controller/AdminController.java
+++ src/com/sap/controller/AdminController.java
@@ -3,16 +3,17 @@ package com.sap.controller;
 
 import com.sap.model.User;
 import com.sap.service.UserService;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.annotation.Resource;
+import javax.validation.Valid;
 
 @Controller
 public class AdminController {
@@ -20,53 +21,84 @@ public class AdminController {
     @Resource
     private UserService userService;
 
-    @RequestMapping(value = "/adminPageTest")
+    @RequestMapping(value = "/admin")
     public String listUsers (Model model){
 
         User admin = userService.getCurrentUser();
-        
+
         model.addAttribute("user", new User());
         model.addAttribute("users", userService.getTeamMates(admin));
-        return "adminPageTest";
+        model.addAttribute("admin", admin);
+        return "admin";
     }
 
     @RequestMapping(value = "/user/add", method = RequestMethod.POST)
-    public String addUser (@ModelAttribute("user") User user){
+    public String addUser (@ModelAttribute("user") User user, Model model){
+
+        if (!userService.verifyIfActionCanBeAppliedToUser(user)) {
+            model.addAttribute("user", userService.getCurrentUser());
+            return "accessDenied";
+        }
+
 
         userService.saveUserAtOwnerTeam(user, userService.getCurrentUser());
 
-        return "redirect:/adminPageTest";
+        return "redirect:/admin";
 
     }
 
     @RequestMapping(value = "remove/{id}")
-    public String removeUser (@PathVariable("id") Integer id){
+    public String removeUser (@PathVariable("id") Integer id, Model model){
 
-        if (userService.verifyIfActionCanBeAppliedToUser(userService.getUserByID(id))) {
-
-            userService.deleteUserByID(id);
-
+        if (!userService.verifyIfActionCanBeAppliedToUser(userService.getUserByID(id))) {
+            model.addAttribute("user", userService.getCurrentUser());
+            return "accessDenied";
         }
-        return "redirect:/adminPageTest";
+
+
+        userService.deleteUserByID(id);
+
+
+        return "redirect:/admin";
     }
 
     @RequestMapping(value = "/edit/{id}")
     public String editUser (@PathVariable("id") Integer id, Model model){
+
+        if (!userService.verifyIfActionCanBeAppliedToUser(userService.getUserByID(id))) {
+            model.addAttribute("user", userService.getCurrentUser());
+            return "accessDenied";
+        }
+
         model.addAttribute("user", userService.getUserByID(id));
         model.addAttribute("users", userService.getTeamMates(userService.getCurrentUser()));
 
-        return "adminPageTest";
+
+
+        return "admin";
     }
 
-    private String getPrincipal(){
-        String userName = null;
-        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+    @RequestMapping(value = "/newUser", method = RequestMethod.POST)
+    public String saveRegistration (@Valid User user, BindingResult result, ModelMap model){
 
-        if (principal instanceof UserDetails) {
-            userName = ((UserDetails)principal).getUsername();
-        } else {
-            userName = principal.toString();
+        User admin = userService.getCurrentUser();
+
+        if (user.getId() != null) {
+            User search = userService.getUserByID(user.getId());
+
+
+                search.setPassword(user.getPassword());
+                search.setSsoId(user.getSsoId());
+                userService.saveUserAtOwnerTeam(search, admin);
+                model.addAttribute("success", "User has been registered successfully");
+                return "redirect:/admin";
+
         }
-        return userName;
+
+        userService.saveUserAtOwnerTeam(user, admin);
+
+        model.addAttribute("success", "User has been registered successfully");
+        return "redirect:/admin";
     }
+
 }
