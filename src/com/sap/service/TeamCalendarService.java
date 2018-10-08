de9819eb594758fce65f2e17b2cdebcf1fd6ee41
Rentz <derick.rentz@sap.com>
2018-10-08 10:00:29 -0300

    Added TeamCalendarService interface, with methods to save, fetch information, create new calendar and verify if date range is possible

---
+++ src/com/sap/service/TeamCalendarService.java
@@ -0,0 +1,23 @@
+package com.sap.service;
+
+import com.sap.model.Team;
+import com.sap.model.TeamCalendar;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import java.util.List;
+
+public interface TeamCalendarService {
+
+    void save (TeamCalendar calendar);
+
+    TeamCalendar getCalendarByID (Integer id);
+
+    List<TeamCalendar> getAllCalendars();
+
+    boolean verifyDate (TeamCalendar teamCalendar, Team team);
+
+    void createCalendar (TeamCalendar calendar);
+
+    boolean isCalendarDatePossible (TeamCalendar calendar, List<TeamCalendar> teamCalendars);
+
+}
