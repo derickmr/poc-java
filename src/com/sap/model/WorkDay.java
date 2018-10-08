855c31923c38325b54da6dc8d1cc801b5d039324
Rentz <derick.rentz@sap.com>
2018-10-07 22:59:24 -0300

    Added WorkDay model, which represents a day of work to an User of a Team

---
+++ src/com/sap/model/WorkDay.java
@@ -0,0 +1,64 @@
+package com.sap.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "WORK_DAY")
+public class WorkDay {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    Integer id;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Day getDay() {
+        return day;
+    }
+
+    public void setDay(Day day) {
+        this.day = day;
+    }
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "USER_WORK_DAY")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "DAY_OF_MONTH_WORK_DAY")
+    private Day day;
+
+    private String shift;
+
+    private boolean canWorkAtHolidayOrWeekend;
+
+    public boolean isCanWorkAtHolidayOrWeekend() {
+        return canWorkAtHolidayOrWeekend;
+    }
+
+    public void setCanWorkAtHolidayOrWeekend(boolean canWorkAtHolidayOrWeekend) {
+        this.canWorkAtHolidayOrWeekend = canWorkAtHolidayOrWeekend;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getShift() {
+        return shift;
+    }
+
+    public void setShift(String shift) {
+        this.shift = shift;
+    }
+}
