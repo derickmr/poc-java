153ecae403f9ed08c05a129f77394479c8147a1b
Rentz <derick.rentz@sap.com>
2018-10-15 16:21:03 -0300

    Added Message entity which is related to one Team

---
+++ src/com/sap/model/Message.java
@@ -0,0 +1,47 @@
+package com.sap.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "MESSAGE")
+public class Message {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    private String message;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "TEAM_MESSAGE")
+    private Team team;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Team getTeam() {
+        return team;
+    }
+
+    public void setTeam(Team team) {
+        this.team = team;
+    }
+
+    @Override
+    public String toString (){
+        return getMessage();
+    }
+}
